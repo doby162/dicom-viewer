51d0efd251fe050ee307d05ef126e93ce71838cf
Michael Dorian <doby162@gmail.com>
2020-01-12 15:39:49 -0600

    display sorted images
    it turns out that django doesn't typically handle serving
    assets like images, leaving that to webservers. As this
    is a puzzle and not a production environment, I didn't want
    to set that up, and since the build it webserver refused
    to serve images, I fell back on base64 encoding them.
    
    Although this is technically bypassing a security mesure by allowing
    static files to be servedi by django, I think it maintains the spirit of
    the restriction. By not allowing files to be selected in any way other than
    by selecting an image series, we still prevent access to
    all information by default, thus protecting source code
    and sensitive data

--- slicer/views.py
+++ slicer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import os
 from .models import ImageSeries
+import base64
 
 
 def image_series_list(request):
@@ -21,9 +22,14 @@ def image_viewer(request):
     id = id.split("'")[0]
 
     images = os.listdir("media/image_dumps/" + id)
+    images.sort()
+    base64_data = []
+    for image in images:
+        with open("media/image_dumps/" + id + "/" + image, "rb") as image_file:
+            data_uri = base64.b64encode(image_file.read()).decode("ascii")
+            base64_data.append('data:image/png;base64,{0}'.format(data_uri))
 
     return render(request, 'image_viewer.html', {
         #folder name for specific series
-        "folder":id,
-        "images":images,
+        "images":base64_data,
     })
